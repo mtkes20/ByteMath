44b2b41aad86f9144e6b085498d2269d3e50f139
mtkes20 <mtkes20@freeuni.edu.ge>
2024-04-08 20:16:10 +0400

    add backend service for bytemath

---
+++ bytemath-service/src/main/java/ge/freeuni/bytemathservice/BytemathServiceApplication.java
@@ -0,0 +1,13 @@
+package ge.freeuni.bytemathservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytemathServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BytemathServiceApplication.class, args);
+    }
+
+}
---
+++ bytemath-service/src/test/java/ge/freeuni/bytemathservice/BytemathServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.freeuni.bytemathservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytemathServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
