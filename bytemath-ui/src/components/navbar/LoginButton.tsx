d80ab2a422efa71bef76f29ac228c4435e2f5f49
mtkes20 <mtkes20@freeuni.edu.ge>
2024-05-11 22:09:22 +0400

    setup keycloak

--- bytemath-ui/src/components/navbar/LoginButton.tsx
+++ bytemath-ui/src/components/navbar/LoginButton.tsx
@@ -1,11 +1,38 @@
+import React from 'react';
 import {Button} from "@mui/material";
+import Keycloak from "keycloak-js"; // make sure this path is correct
 
+interface KeycloakOptions {
+    url: string;
+    realm: string;
+    clientId: string;
+    onLoad: Keycloak.KeycloakOnLoad;
+}
 
-const LoginButton = () => {
+let initOptions: KeycloakOptions = {
+    url: 'http://localhost:8080/',
+    realm: 'master',
+    clientId: 'bytemath',
+    onLoad: 'login-required'
+}
 
+const keycloak: Keycloak = new Keycloak(initOptions);
+
+const LoginButton: React.FC = () => {
+    const handleLogin = () => {
+        keycloak.init({ onLoad: initOptions.onLoad}).then((auth: boolean) => {
+            if (!auth) {
+                console.error("Not Authenticated");
+            } else {
+                console.info("Authenticated");
+            }
+        }).catch(() => {
+            console.error("Authentication Failed");
+        });
+    };
 
     return (
-        <Button style={{
+        <Button onClick={handleLogin} style={{
             backgroundColor: "#800080",
             padding: "5px 10px",
             color: "#ffffff",
@@ -13,7 +40,7 @@ const LoginButton = () => {
         }}>
             Sign In
         </Button>
-    )
+    );
 }
 
 export default LoginButton;
---
+++ keycloak/themes/custom-theme/theme/custom/login/login.ftl
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="${url.resourcesPath}/css/styles.css" />
+</head>
+<body>
+<div id="kc-container">
+    <div id="kc-container-wrapper">
+        <div id="kc-header">
+            <h1>Welcome</h1>
+        </div>
+        <form id="kc-form" action="${url.loginAction}" method="post">
+            <div id="kc-form-fields">
+                <input type="text" name="username" placeholder="Username" required autofocus>
+                <input type="password" name="password" placeholder="Password" required>
+                <input type="submit" value="Sign In">
+            </div>
+            <div class="kc-form-options">
+                <a class="kc-forgot-password">Forgot Password?</a><br>
+                <a class="kc-register">Register</a>
+            </div>
+        </form>
+    </div>
+</div>
+</body>
+</html>
