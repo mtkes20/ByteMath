aa63a70a56b71278b516e66e0db3af01dd48ac26
teklabasilidze <tekle.basilidze@azry.com>
2024-07-21 15:45:59 +0400

    Add language param to quiz api

--- bytemath-ui/src/api/quiz-api.ts
+++ bytemath-ui/src/api/quiz-api.ts
@@ -4,22 +4,24 @@ import {SubmittedQuiz} from "../types/SubmittedQuiz";
 import {GradedQuiz} from "../types/GradedQuiz";
 
 
-const getQuiz = async (quizId: string): Promise<QuizType> => {
+const getQuiz = async (quizId: string, language: string): Promise<QuizType> => {
     const response = await axios.create({
         baseURL: "/api/v1/quiz/",
         headers: {
             'Content-Type': 'application/json',
-        }}).get(`${quizId}`);
+        },
+    }).get(`${quizId}?language=${language}`);
 
     return response.data;
 }
 
+//TODO pass correct language
 const submitQuiz = async (quizId: string, answers: SubmittedQuiz): Promise<GradedQuiz> => {
     const response = await axios.create({
         baseURL: "/api/v1/quiz/",
         headers: {
             'Content-Type': 'application/json',
-        }}).post(`${quizId}/submit`, answers);
+        }}).post(`${quizId}/submit?language=GEO`, answers);
 
     return response.data;
 }
