cf897e240acb1371eadfe8ef0160d7624482a6d4
mtkes20 <mtkes20@freeuni.edu.ge>
2024-08-27 00:20:20 +0400

    add course entity in the system

--- bytemath-service/src/main/java/ge/freeuni/bytemathservice/domain/entity/Course.java
+++ bytemath-service/src/main/java/ge/freeuni/bytemathservice/domain/entity/Course.java
@@ -1,13 +1,11 @@
 package ge.freeuni.bytemathservice.domain.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
@@ -20,25 +18,27 @@ import java.util.Set;
 @Getter
 @Setter
 @Entity
-@Table(name = "PAGE", schema = "BYTEMATH")
+@Table(name = "COURSE", schema = "BYTEMATH")
 @EqualsAndHashCode(of = "id")
-public class Page {
+public class Course {
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_generator")
     @SequenceGenerator(name = "seq_generator", schema = "BYTEMATH", sequenceName = "BYTEMATH_GLOBAL_SEQUENCE", allocationSize = 1)
     private Long id;
 
-    private String identifier;
+    private String name;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "COURSE_ID")
-    private Course course;
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Page> pages = new HashSet<>();
 
-    @ManyToMany(mappedBy = "readPages")
-    private Set<BytemathUser> readBy = new HashSet<>();
+    public void addPage(Page page) {
+        pages.add(page);
+        page.setCourse(this);
+    }
 
-    public void markAsReadBy(BytemathUser bytemathUser) {
-        this.readBy.add(bytemathUser);
-        bytemathUser.getReadPages().add(this);
+    public void removePage(Page page) {
+        pages.remove(page);
+        page.setCourse(null);
     }
 }
